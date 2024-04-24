71fd46b6f3de1da2b42860c2ef2228097cf56123
Gopalakrishnan <krishnan.gopal34@gmail.com>
2024-04-24 16:12:19 +0530

    refactor: :art: react router dom added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useRouteMatch,
+} from "react-router-dom";
+
+import "./App.css";
+import { Login } from "./login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Router>
+        <Switch>
+          <Route path="/polls">
+            <HomePage />
+          </Route>
+          <Route path="/poll">
+            <PollPage />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </main>
   );
 }
+
+const HomePage = () => {
+  return <div>Home Page</div>;
+};
+
+const PollPage = () => {
+  let match = useRouteMatch();
+
+  return (
+    <Switch>
+      <Route path={`${match.path}/:pollId/response`}>
+        <ResponsePage />
+      </Route>
+      <Route path={`${match.path}/:pollId`}>
+        <BuilderPage />
+      </Route>
+    </Switch>
+  );
+};
+
+const BuilderPage = () => {
+  return <div>Poll builder page</div>;
+};
+
+const ResponsePage = () => {
+  return <div>Response Page</div>;
+};
 
 export default App;
